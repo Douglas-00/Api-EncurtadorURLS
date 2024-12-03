b4c6bfd78f63a3258166979d0d6838e68a43f24b
Douglas-00 <alvesdouglas547@gmail.com>
2024-12-03 17:20:05 -0300

    feat: config swagger
    instalando swagger e configurando

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Config Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Encurtador de URL')
+    .setDescription('APi de encurtador de URL')
+    .setVersion('1.0')
+    .addTag('encurtador')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
