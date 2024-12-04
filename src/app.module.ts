5cc2d8a9ce66e62b01cc842e6263474455e9f3a2
Douglas-00 <alvesdouglas547@gmail.com>
2024-12-04 18:26:26 -0300

    feat: importando o user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './db/prisma.module';
 import { LoggerModule } from './modules/logger/logger.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [PrismaModule, LoggerModule],
+  imports: [PrismaModule, LoggerModule, UserModule],
   controllers: [],
   providers: [],
 })
